be0e281332a4fda9c6f842fa8393c15a9de0194a
Leo Zhou <leoyihanzhou@gmail.com>
2025-03-25 16:58:28 -0700

    initial drivetrain code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,11 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Drivetrain.Drive;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -21,15 +24,22 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Drivetrain drivetrain = new Drivetrain();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
+  private final CommandJoystick leftStick = new CommandJoystick(0);
+  private final CommandJoystick rightStick = new CommandJoystick(1);
+
+  private final Drive drive = drivetrain.new Drive(drivetrain, leftStick, rightStick);
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+    drivetrain.setDefaultCommand(drive);
   }
 
   /**
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,58 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class Drivetrain extends SubsystemBase {
+    private TalonFX leftMotor1, leftMotor2;
+    private TalonFX rightMotor1, rightMotor2;
+
+    public Drivetrain() {
+        leftMotor1 = new TalonFX(1);
+        leftMotor2 = new TalonFX(2);
+        rightMotor1 = new TalonFX(3);
+        rightMotor2 = new TalonFX(4);
+
+        //set motors inverted in config idk
+    }
+
+    public void drive(double leftSpeed, double rightSpeed) {
+        leftMotor1.set(leftSpeed);
+        leftMotor2.set(leftSpeed);
+        rightMotor1.set(rightSpeed);
+        rightMotor2.set(rightSpeed);
+    }
+
+    public void stop() {
+        leftMotor1.stopMotor();
+        leftMotor2.stopMotor();
+        rightMotor1.stopMotor();
+        rightMotor2.stopMotor();
+    }
+
+    public class Drive extends Command {
+        private Drivetrain drivetrain;
+        private CommandJoystick leftStick, rightStick;
+
+        public Drive(Drivetrain drivetrain, CommandJoystick leftStick, CommandJoystick rightStick) {
+            this.drivetrain = drivetrain;
+            this.leftStick = leftStick;
+            this.rightStick = rightStick;
+
+            addRequirements(drivetrain);
+        } 
+
+        @Override
+        public void execute() {
+            drivetrain.drive(leftStick.getY(), rightStick.getY());
+        }
+        
+        @Override
+        public void end(boolean interrupted) {
+            drivetrain.stop();
+        }
+    }
+}
