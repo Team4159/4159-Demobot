db00d0b11c5563246c2199eb716000da89b42091
Ethan <etzhu1@gmail.com>
2025-09-25 17:14:28 -0700

    tidy, prepare for vision and auto align

---
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Vision extends SubsystemBase {
+    public final String limelightKey = "limelight";
+    private final NetworkTable limelight = NetworkTableInstance.getDefault().getTable(limelightKey);
+    public boolean hasTarget;
+    public double offsetX;
+    public double area;
+
+    public Vision() {
+        hasTarget = false;
+        area = 0;
+    }
+
+    @Override
+    public void periodic() {
+        offsetX = limelight.getEntry("tx").getDouble(0);
+        area = limelight.getEntry("ta").getDouble(0);
+    }
+}
