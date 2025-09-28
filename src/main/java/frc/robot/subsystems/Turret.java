2782f6be9ec5e8755262fdd838b19ffac37e3b93
breathaning <etzhu1@gmail.com>
2025-09-27 21:22:50 -0700

    add pid

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -2,19 +2,23 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
-
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.LimelightHelpers;
 import frc.robot.Constants.TurretConstants.TurretState;
 
 public class Turret extends SubsystemBase {
-    private SparkMax turretMotor;
+    final private SparkMax turretMotor = new SparkMax(Constants.TurretConstants.kTurretMotorId, MotorType.kBrushless);
+    final private Constraints turretPidConstraints = new Constraints(10, 10);
+    final private ProfiledPIDController turretPid = new ProfiledPIDController(3, 0.1, 0.1, turretPidConstraints);
 
     public Turret() {
-        turretMotor = new SparkMax(Constants.TurretConstants.kTurretMotorId, MotorType.kBrushless);
+
     }
 
     public class TurnTurret extends Command {
@@ -25,24 +29,25 @@ public class Turret extends SubsystemBase {
         }
 
         @Override
-        public void initialize() {
-            turretMotor.set(turretState.motorSpeed);
+        public void execute() {
+            // do nothing
         }
     }
 
     public class AutoAlign extends Command {
+        private double goalX;
+
         public AutoAlign() {
-
         }
-
+        
         @Override
         public void initialize() {
-
+            goalX = turretMotor.getEncoder().getPosition() - LimelightHelpers.getTX("limelight");
         }
 
         @Override
         public void execute() {
-
+            turretMotor.set(turretPid.calculate(turretMotor.getEncoder().getPosition(), goalX));
         }
 
         @Override
