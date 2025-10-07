19ee600d629ab57f7b38277a461894e8c22cd6e4
Ethan <etzhu1@gmail.com>
2025-10-06 17:27:37 -0700

    this better work

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -2,19 +2,24 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
-
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.LimelightHelpers;
 import frc.robot.Constants.TurretConstants.TurretState;
 
 public class Turret extends SubsystemBase {
-    private SparkMax turretMotor;
+    final private SparkMax turretMotor = new SparkMax(Constants.TurretConstants.kTurretMotorId, MotorType.kBrushless);
+    final private Constraints turretPidConstraints = new Constraints(10, 10);
+    final private ProfiledPIDController turretPid = new ProfiledPIDController(3, 0.1, 0.1, turretPidConstraints);
 
     public Turret() {
-        turretMotor = new SparkMax(Constants.TurretConstants.kTurretMotorId, MotorType.kBrushless);
+
     }
 
     public class TurnTurret extends Command {
@@ -25,24 +30,25 @@ public class Turret extends SubsystemBase {
         }
 
         @Override
-        public void initialize() {
-            turretMotor.set(turretState.motorSpeed);
+        public void execute() {
+            // do nothing
         }
     }
 
     public class AutoAlign extends Command {
+
         public AutoAlign() {
-
         }
-
+        
         @Override
         public void initialize() {
-
         }
 
         @Override
         public void execute() {
-
+            double error = Units.degreesToRadians(LimelightHelpers.getTX("limelight"));
+            double position = turretMotor.getEncoder().getPosition();
+            turretMotor.set(turretPid.calculate(position, position + error));
         }
 
         @Override
@@ -52,7 +58,7 @@ public class Turret extends SubsystemBase {
 
         @Override
         public void end(boolean interrupted) {
-            
+            System.out.println("help");
         }
     }
 }
