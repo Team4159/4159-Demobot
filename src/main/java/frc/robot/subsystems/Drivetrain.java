de388c860bdb2b41f94310c6fce1b52bcec3a82f
breathaning <etzhu1@gmail.com>
2025-11-03 20:03:47 -0800

    improve magnitude calculations and add comments

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -45,24 +45,22 @@ public class Drivetrain extends SubsystemBase {
     }
 
     public class Drive extends Command {
-        private Drivetrain drivetrain;
         private CommandXboxController controller;
 
-        public Drive(Drivetrain drivetrain, CommandXboxController controller) {
-            this.drivetrain = drivetrain;
+        public Drive(CommandXboxController controller) {
             this.controller = controller;
 
-            addRequirements(drivetrain);
+            addRequirements(Drivetrain.this);
         }
 
         @Override
         public void execute() {
-            drivetrain.drive(controller.getLeftY(), controller.getRightY());
+            Drivetrain.this.drive(controller.getLeftY(), controller.getRightY());
         }
 
         @Override
         public void end(boolean interrupted) {
-            drivetrain.stop();
+            Drivetrain.this.stop();
         }
     }
 
@@ -79,13 +77,17 @@ public class Drivetrain extends SubsystemBase {
             double inputX = controller.getLeftX();
             double inputY = controller.getLeftY();
             double direction = Math.signum(inputY);
-            double magnitude = Math.hypot(inputX, inputY);
+            double rawMagnitude = Math.hypot(inputX, inputY);
+            // adjusts range to start at 0 instead of starting at the deadzone
+            double correctedMagnitude = Math.max(0,
+                    (rawMagnitude - ArcadeDriveConstants.kInputDeadzone) / (1 - ArcadeDriveConstants.kInputDeadzone));
 
+            // absolute angles
             double angleFromHorizontal = Math.atan2(Math.abs(inputY), Math.abs(inputX));
             double angleFromVertical = Math.abs(Units.degreesToRadians(90) - Math.abs(angleFromHorizontal));
 
             double leftDirection = 0, rightDirection = 0;
-            if (magnitude < ArcadeDriveConstants.kInputDeadzone) {
+            if (rawMagnitude < ArcadeDriveConstants.kInputDeadzone) {
                 // do nothing
             } else if (angleFromVertical <= ArcadeDriveConstants.kTranslationBufferAngle) {
                 leftDirection = direction;
@@ -95,13 +97,22 @@ public class Drivetrain extends SubsystemBase {
                 if (angleFromHorizontal <= ArcadeDriveConstants.kRotationBufferAngle) {
                     rotationAlpha = 0;
                 } else {
-                    double range = Units.degreesToRadians(90) - ArcadeDriveConstants.kRotationBufferAngle
-                            - ArcadeDriveConstants.kTranslationBufferAngle;
-                    rotationAlpha = direction
-                            * ((angleFromHorizontal - ArcadeDriveConstants.kRotationBufferAngle) / range);
+                    // range of analog motion that is outside of the buffer zones
+                    double angleRange = Units.degreesToRadians(90) - (ArcadeDriveConstants.kRotationBufferAngle
+                            + ArcadeDriveConstants.kTranslationBufferAngle);
+                    double relativeAngle = angleFromHorizontal - ArcadeDriveConstants.kRotationBufferAngle;
+                    rotationAlpha = direction * (relativeAngle / angleRange); // range: [-1, 1]
                 }
+
+                // get directions
+                // 90 to 0 degrees: lerp from (-1, -1) to (-1, 1)
+                // 0 to -90 degrees: lerp from (-1, 1) to (1, 1)
                 leftDirection = -1 + Math.max(0, rotationAlpha * 2);
+                leftDirection = Math.min(1, Math.max(-1, leftDirection));
                 rightDirection = -1 + ((rotationAlpha + 1) * 2);
+                rightDirection = Math.min(1, Math.max(-1, rightDirection));
+
+                // reverse inputs if the input is leftward
                 if (inputX < 0) {
                     double temp = leftDirection;
                     leftDirection = rightDirection;
@@ -109,12 +120,13 @@ public class Drivetrain extends SubsystemBase {
                 }
             }
 
-            double left = leftDirection * magnitude;
-            left = Math.min(1, Math.max(-1, left));
-            double right = rightDirection * magnitude;
-            right = Math.min(1, Math.max(-1, right));
+            // get speeds based off direction and input magnitude
+            double leftSpeed = leftDirection * correctedMagnitude;
+            leftSpeed = Math.min(1, Math.max(-1, leftSpeed));
+            double rightSpeed = rightDirection * correctedMagnitude;
+            rightSpeed = Math.min(1, Math.max(-1, rightSpeed));
 
-            Drivetrain.this.drive(left, right);
+            Drivetrain.this.drive(leftSpeed, rightSpeed);
         }
     }
 }
